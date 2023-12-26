e869e7cf8a056dd06728014d7cff712c20be26f7
Borti0 <barticel.vlad@gmail.com>
2023-12-27 00:54:03 +0200

    Update json config file and emulation camera module.
    Now we have 2 emulated cameras:
    - wide front camera view (1580x970 resolution)
    - fish eye camera view (970x970 resolution)
    
    Now we can select to resize the emulated footage, make it B/W, crop the footage.
    TO DO: implement emulated fps in the emulated camera function.

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import cv2
 from utils.lists import DLListLimit
-
-
+from utils.emulated_camera import EmuCam
+import json
+import time
 
 if __name__ == "__main__":
-    print(cv2.getVersionMajor())
+    print(cv2.__version__)
 
-    test_list = DLListLimit(10, -3)
+    test_list = DLListLimit(10, 0)
 
-    for i in range(0, 99, 1):
+    for i in range(0, 25, 1):
         test_list.add_to_front(i)
+        print(f"iteration: {i} ", end="")
         if test_list.is_half is True:
-            print(f"List value is {test_list.read_data()}")
+            print(f"List value is {test_list.read_data()}", end="")
+        print("")
+
+    camera_pool = open("./configs/emu_cam.json", "r")
+    cameras_configs = json.load(camera_pool)
+    used_camera = cameras_configs["used_cam"]
+    used_camera_config = cameras_configs["cameras"][used_camera]
+    print(used_camera_config)
+    
+    emu_cam = EmuCam(used_camera_config)
+
+    if emu_cam.get_camera_status() is False:
+        print("Error to init emulated camera")
+        exit(-1)
+
+    cv2.startWindowThread()
+    cv2.namedWindow("show")
+
+    while True:
+        frame = emu_cam.emulate_camera()
+        if frame is None and emu_cam.get_camera_status() is False:
+            print("error to read frame")
+            exit(-1)
+        cv2.imshow("show", frame)
+        key = cv2.waitKey(1)
+        if key == 'q':
+            break
+        print(emu_cam.get_playback_procentage())
+
+    emu_cam.stop_camera()
+    cv2.destroyAllWindows()
+
