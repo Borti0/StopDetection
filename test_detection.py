3d5d2ffb63489025a909130c65901405fce20bc3
Borti0 <barticel.vlad@gmail.com>
2023-12-30 22:55:57 +0200

    Full detection of stop sign based on edge detection via diagonal matrix.

--- test_detection.py
+++ test_detection.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
 
     #open image
     #cv2.namedWindow("original_img")
-    frame = cv2.imread("./videos/sample_wide_cam4.png")
+    frame = cv2.imread("./videos/sample_wide_cam5.png")
     old_cols = frame.shape[1]
     old_lines = frame.shape[0]
     frame = frame[
-        0:old_lines-115, int(old_cols/2):
+        0:old_lines-100, int(old_cols/2):
     ]
     #cv2.imshow("original_img", frame)
     #cv2.waitKey(1)
@@ -35,20 +35,23 @@ if __name__ == "__main__":
     #time.sleep(10)
 
     #rank filters
-    RX = numpy.ones((2, 8)) / 16
-    Rx = numpy.ones((3, 25)) / 75
+    RX = numpy.ones((2, 9)) / 27
+    Rx = numpy.ones((3, 35)) / 105
     Ry = numpy.transpose(Rx)
     #print(Rx)
     #print(Ry)
     frame_bx = cv2.filter2D(src=frame_bw, ddepth=-1, kernel=Rx)
     frame_by = cv2.filter2D(src=frame_bw, ddepth=-1, kernel=Ry)
-    tmp_f = cv2.filter2D(src=frame_by, ddepth=-1, kernel=RX)
-    frame_by = tmp_f
+    #tmp_f = cv2.filter2D(src=frame_by, ddepth=-1, kernel=RX)
+    #frame_by = tmp_f
     #cv2.imshow("bw", frame_bx)
     #cv2.waitKey(1)
 
     #edge detection
-    Gx = numpy.array([[-1, -2, -1],[0,0,0],[1, 2, 1]])
+    Gx = numpy.array([
+        [-4,  2,  0],
+        [ 2,  0, -2],
+        [ 0, -2,  4] ])
     #print(Gx)
     Gy = Gx.transpose()
     #print(Gy)
@@ -71,7 +74,7 @@ if __name__ == "__main__":
     #matplotlib.pyplot.hist(h, bins=interv)
     #matplotlib.pyplot.show()
     hc = numpy.cumsum(h)
-    procent = 0.95
+    procent = 0.99
     poz = numpy.where(hc >= procent * lines * colums)
     #print(poz)
     poz = poz[0][0]
@@ -121,9 +124,62 @@ if __name__ == "__main__":
     frame = frame[
         prag1:prag2, :
     ]
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    frame_bx = cv2.filter2D(src=frame_bw, ddepth=-1, kernel=Rx)
+
+    Imx = cv2.filter2D(src=frame_bw, ddepth=-1, kernel=Gx)
+    Imy = cv2.filter2D(src=frame_bw, ddepth=-1, kernel=Gy)
+
+    Im = abs(Imx) + abs(Imy)
+    new_col = frame_bx.shape[1]
+
+    Px = np.zeros((1, new_col))
+    Px = Px.transpose()
+    print(len(Im[0]), len(Im), new_col)
+    for i in range(0, new_col-1, 1):
+        Px[i] = sum(Im[:, i])
+
+    matplotlib.pyplot.plot(Px)
+    matplotlib.pyplot.show()
+
+    b, a = scipy.signal.butter(5, 0.05, "low")
+    Pxn = scipy.signal.filtfilt(b, a, Px.transpose())
+
+    Pxn = Pxn.transpose()
+
+    matplotlib.pyplot.plot(Pxn)
+    matplotlib.pyplot.show()
+
+    cv2.imshow("bw", Im)
+    cv2.waitKey(60)
+    time.sleep(5)
+
+    max_hist = Pxn.max()
+    poz_max_hist = Pxn.argmax()
+    print(max_hist, poz_max_hist)
+
+    prag1 = 0
+    for i in range(poz_max_hist - 1, 1, -1):
+        if Pxn[i] < Pxn[i - 1]:
+            prag1 = i
+            break
+
+    prag2 = 0
+    for i in range(poz_max_hist + 1, len(Pyn) - 1, 1):
+        if Pxn[i] < Pxn[i + 1]:
+            prag2 = i
+            break
+    print(prag1, prag2)
+
+    frame = frame[
+        :, prag1:prag2
+    ]
+
     cv2.imshow("bw", frame)
     cv2.waitKey(60)
     time.sleep(5)
+
     input()
 
     cv2.destroyAllWindows()
