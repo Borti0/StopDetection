a454efa6cf007bf652c522e952507286b534139d
Borti0 <barticel.vlad@gmail.com>
2023-12-30 20:24:02 +0200

    Finish adjusting window by lines
    For some Stop signs the window is following, for that one's which between trees is not working.

--- test_detection.py
+++ test_detection.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     old_cols = frame.shape[1]
     old_lines = frame.shape[0]
     frame = frame[
-        0:old_lines-100, int(old_cols/2):
+        0:old_lines-115, int(old_cols/2):
     ]
     #cv2.imshow("original_img", frame)
     #cv2.waitKey(1)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     #time.sleep(10)
 
     #rank filters
-    RX = numpy.ones((4, 25)) / 100
+    RX = numpy.ones((2, 8)) / 16
     Rx = numpy.ones((3, 25)) / 75
     Ry = numpy.transpose(Rx)
     #print(Rx)
@@ -71,13 +71,13 @@ if __name__ == "__main__":
     #matplotlib.pyplot.hist(h, bins=interv)
     #matplotlib.pyplot.show()
     hc = numpy.cumsum(h)
-    procent = 1
+    procent = 0.95
     poz = numpy.where(hc >= procent * lines * colums)
-    print(poz)
+    #print(poz)
     poz = poz[0][0]
     prag = h[poz]
-    print(prag)
-    print(h)
+    #print(prag)
+    #print(h)
 
     Py = np.zeros((lines, 1))
     for i in range(0, lines, 1):
@@ -86,11 +86,44 @@ if __name__ == "__main__":
             Py[i] = tmp
     matplotlib.pyplot.plot(Py)
     matplotlib.pyplot.show()
-    cv2.namedWindow("crop")
-    frame_c = frame[398:493, :]
-    cv2.imshow("crop", frame_c)
-    cv2.waitKey(1)
+    #cv2.namedWindow("crop")
+    #frame_c = frame[650:700, :]
+    #cv2.imshow("crop", frame_c)
+    #cv2.waitKey(1)
 
+    # filtru butter
+    b,a = scipy.signal.butter(5, 0.05, "low")
+    Pyn = scipy.signal.filtfilt(b,a, Py.transpose())
+
+    Pyn = Pyn.transpose()
+
+    matplotlib.pyplot.plot(Pyn)
+    matplotlib.pyplot.show()
+
+    max_hist = Pyn.max()
+    poz_max_hist = Pyn.argmax()
+    print(max_hist, poz_max_hist)
+
+    prag1 = 0
+    for i in range(poz_max_hist-1, 1,-1):
+        if Pyn[i] < Pyn[i-1]:
+            prag1 = i
+            break
+
+    prag2 = 0
+    for i in range(poz_max_hist + 1, len(Pyn)-1, 1):
+        if Pyn[i] < Pyn[i+1]:
+            prag2 = i
+            break
+    print(prag1, prag2)
+
+    ## -----
+    frame = frame[
+        prag1:prag2, :
+    ]
+    cv2.imshow("bw", frame)
+    cv2.waitKey(60)
+    time.sleep(5)
     input()
 
     cv2.destroyAllWindows()
